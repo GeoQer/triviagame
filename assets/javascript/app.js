4b6a433a1505a018280a2a4d779cd118ae8c0316
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-08-27 21:32:05 -0700

    initial psuedo code setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+//create variables
+var timerQues;
+var timerAns;
+var correct;
+var wrong;
+var reset;
+var countDown;
+var submit;
+
+//create game reset
+
+//create question timer (timerQues)
+
+//create timer display (countDown)
+
+//create questions (objects?)
+
+//create conditional statements
+
+//create correct answer display
+
+//create wrong answer display
+
+//call game reset
