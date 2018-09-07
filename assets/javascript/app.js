cdcba2160e3e2efcc8d2d59ebe794acd881c819a
GeoQer <40548603+GeoQer@users.noreply.github.com>
2018-09-06 17:40:13 -0700

    a little more progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,47 @@
 //create variables
-var timerQues;
-var timerAns;
+var timer;
+var question;
+var answer;
 var correct;
 var wrong;
-var reset;
 var countDown;
 var submit;
+var index;
+var questions;
+var intervalId;
+var decrement;
+var timeOut;
+var ansCorrect;
+var ansWrong;
 
 //create game reset
+function reset(){
+    correct = 0;
+    wrong = 0;
+    index = 0;
+    }
 
 //create question timer (timerQues)
+function question(){
+    timer = 25;
+
+    timerQues = setInterval(countDown, 1000);    
+}
+
+function countDown(){
+    timer--;
+
+    $("#timer").html("<h2>" + timer + "</h2>");
+
+    if (timer === 0) {
+        
+        clearInterval(timerQues);
+
+        $("#timer").html("<h1>Time's Up!</h1>");
+        $("#display").html()
+
+    }
+}
 
 //create timer display (countDown)
 
